cccbd7e86fb2ae1231b9ec6d58ffccf25a695c69
Mark Gortzak <mgortzak@gmail.com>
2018-04-24 11:46:09 +0200

    Added a class.

--- src/app/bot/bot.component.ts
+++ src/app/bot/bot.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Person} from "../domain/person";
 
 @Component({
   selector: 'app-bot',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BotComponent implements OnInit {
 
-  constructor() { }
+  pietje: Person;
+  jantje: Person;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.pietje = new Person();
+    this.pietje.firstName = 'Pietje';
+    this.pietje.lastName = 'Puk';
+    this.pietje.email = 'pietje@puk.nl';
+
+    this.jantje = new Person();
+    this.jantje.firstName = 'Jantje';
+    this.jantje.lastName = 'Beton';
+    this.jantje.email = 'jantje@beton.nl';
+
+    console.log(this.pietje);
+    console.log(this.jantje);
   }
 
 }
